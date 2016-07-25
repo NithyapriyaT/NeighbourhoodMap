9fdaf99d0b6a53f6c3a268a0d516da562abdd9f0
NithyaPriya Thangavelu <NithyaPriya_Thangavelu@NithyaPriyas-MacBook-Pro.local>
2016-07-25 14:06:40 -0400

    Function is added to open a infowindow on clicking the List items.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Declaration of all the necessary variables
 var placeArray = [
 			{ name: 'Corning Museum Of Glass',location:{lat: 42.149839, lng: -77.054048}},
 			{ name: 'Rockwell Museum' ,location:{lat: 42.142627, lng: -77.052871}},
@@ -6,54 +7,72 @@ var placeArray = [
 			{ name: 'Wegmans',location:{lat: 42.149085, lng: -77.060578}},
 			{ name: 'Atlas Pizzeria',location:{lat: 42.1427778, lng: -77.055 }}
 		];
-		ko.applyBindings(placeArray);
-
 var map;
+var marker;
 var markers=[];
+var largeInfowindow;
+var placeArrayLength = placeArray.length;
+//viewmodel
+var viewModel = function(){
+	var self = this;
+	self.placeList = ko.observable(placeArray);
+//Added a function for the click databind to display the infowindow on clicking the list
+	displayInfoWindow = function(place){
+		for(var i=0; i<placeArrayLength; i++){
+			if(place.name === placeArray[i].name){
+				var markerNewRef = markers[i];
+				toggleMarker(markerNewRef);
+				populateInfoWindow(markerNewRef, largeInfowindow);
+			}
+		}
+	}
+};
+ko.applyBindings(new viewModel());	
 
+
+//initMap callback function.
 function initMap() {
+	largeInfowindow = new google.maps.InfoWindow();
     // Constructor creates a new map - only center and zoom are required.
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 42.1427778, lng: -77.055},
           zoom: 15
         }); 
-        for(i=0;i<placeArray.length; i++){
-	        var marker = new google.maps.Marker({
+        for(i=0;i<placeArrayLength; i++){
+	        marker = new google.maps.Marker({
 	            map: map,
 	            position: placeArray[i].location,
 	            title: placeArray[i].name,
 	            animation: google.maps.Animation.DROP
 	        });	
-	        
-	        var largeInfowindow = new google.maps.InfoWindow();
-	        // Push the marker to our array of markers.
 	        markers.push(marker);
-
 	     	// Create an onclick event to open an infowindow at each marker.
 	       	marker.addListener('click', function() {
 				toggleMarker(this);
 	    		populateInfoWindow(this, largeInfowindow);
-				});	 
+			});	 
 		}	
 }
+
 function toggleMarker(marker) {
  	if (marker.getAnimation() !== null) {
- 			//console.log("hello");
     		marker.setAnimation(null);
    	} else {
-    		//console.log("else");
       	marker.setAnimation(google.maps.Animation.DROP);
    	}
 }
 function populateInfoWindow(marker, infowindow) {
         // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
+        if(infowindow.marker != marker) {
           infowindow.marker = marker;
           infowindow.setContent('<div>' + marker.title + '</div>');
           infowindow.open(map, marker);
           // Make sure the marker property is cleared if the infowindow is closed.
           infowindow.addListener('closeclick', function() {
+          	console.log("close");
             infowindow.marker = null;
           });
         }
       }
+
+
