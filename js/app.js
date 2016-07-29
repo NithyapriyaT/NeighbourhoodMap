694f55454b99a8c4139c7eb02d99e0b22b15bacb
NithyaPriya Thangavelu <NithyaPriya_Thangavelu@NithyaPriyas-MacBook-Pro.local>
2016-07-29 13:23:58 -0400

    Added the marker according to the list change.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //Declaration of all the necessary variables
-var placeArray = [
+var locations = [
 			{ name: 'Corning Museum Of Glass',location:{lat: 42.149839, lng: -77.054048}},
 			{ name: 'Rockwell Museum' ,location:{lat: 42.142627, lng: -77.052871}},
 			{ name: 'Corning Bottles and Corks',location:{lat: 42.144192, lng: -77.059808 }},
@@ -11,46 +11,14 @@ var map;
 var marker;
 var markers=[];
 var largeInfowindow;
-var placeArrayLength = placeArray.length;
-var locationList;
-var stringStartsWith = function (string, startsWith) { 
-console.log("hai");        
+var locationsLength = locations.length;
+
+var stringStartsWith = function (string, startsWith) {        
         string = string || "";
         if (startsWith.length > string.length)
             return false;
         return string.substring(0, startsWith.length) === startsWith;
     };
-
-var viewModel = function(){
-//Added a function for the click databind to display the infowindow on clicking the list
-	this.locationList = ko.observable(placeArray);
-
-	this.displayInfoWindow = function(place){
-		for(var i=0; i<placeArrayLength; i++){
-			if(place.name === placeArray[i].name){
-				var markerNewRef = markers[i];
-				toggleMarker(markerNewRef);
-				populateInfoWindow(markerNewRef, largeInfowindow);
-			}
-		}
-	};
-
-	this.queryThePlaces = ko.observable('');
-
-	this.query = ko.computed(function(){
-		var queryThePlaces = this.queryThePlaces().toLowerCase();
-		if(!queryThePlaces) {
-			return this.locationList();
-		}
-		else {
-			return ko.utils.arrayFilter(this.locationList(), function(list) {
-            	return stringStartsWith(list.name.toLowerCase(), queryThePlaces);	
-        	});
-		}	
-	},this);
-};
-ko.applyBindings(new viewModel());	
-
 
 //initMap callback function.
 function initMap() {
@@ -60,14 +28,16 @@ function initMap() {
           center: {lat: 42.1427778, lng: -77.055},
           zoom: 15
         }); 
-        for(i=0;i<placeArrayLength; i++){
-        	//console.log("inside marker")
+        for(i=0;i<locationsLength; i++){
 	        marker = new google.maps.Marker({
 	            map: map,
-	            position: placeArray[i].location,
-	            title: placeArray[i].name,
+	            position: locations[i].location,
+	            title: locations[i].name,
 	            animation: google.maps.Animation.DROP
 	        });	
+	        //create a marker property for the locations array to store the every marker for their associated locations
+	        locations[i].marker = marker;
+	 		//push the marker to the markers array.
 	        markers.push(marker);
 	     	// Create an onclick event to open an infowindow at each marker.
 	       	marker.addListener('click', function() {
@@ -75,6 +45,7 @@ function initMap() {
 	    		populateInfoWindow(this, largeInfowindow);
 			});	 
 		}	
+		ko.applyBindings(new viewModel());
 }
 
 function toggleMarker(marker) {
@@ -92,10 +63,45 @@ function populateInfoWindow(marker, infowindow) {
           infowindow.open(map, marker);
           // Make sure the marker property is cleared if the infowindow is closed.
           infowindow.addListener('closeclick', function() {
-          	console.log("close");
             infowindow.marker = null;
           });
         }
       }
+var viewModel = function(){
+	this.locations = ko.observable(locations);
+//Added a function for the click databind to display the infowindow on clicking the list
+	this.displayInfoWindow = function(place){
+		for(var i=0; i<locationsLength; i++){
+			if(place.name === locations[i].name){
+				var markerNewRef = markers[i];
+				toggleMarker(markerNewRef);
+				populateInfoWindow(markerNewRef, largeInfowindow);
+			}
+		}
+	};
 
+	this.queryThePlaces = ko.observable('');
 
+	this.query = ko.computed(function(){
+
+		var queryThePlaces = this.queryThePlaces().toLowerCase();
+		if(!queryThePlaces) {
+			for(i=0;i<markers.length;i++){	
+	            markers[i].setVisible(true);
+            }
+			return this.locations();
+		}
+		else {
+			return ko.utils.arrayFilter(this.locations(), function(list) {
+            	var result = stringStartsWith(list.name.toLowerCase(), queryThePlaces);
+            	if(result == true){
+            		list.marker.setVisible(true);
+            	}
+            	else{
+            		list.marker.setVisible(false);
+				}
+        		return result;
+        	});
+		}	
+	},this);
+};
